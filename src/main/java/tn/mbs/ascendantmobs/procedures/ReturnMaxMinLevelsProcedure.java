c18c09d8dfd8a6b81537d496bd28295ce1e35c18
MeherBenSalem <74470806+MeherBenSalem@users.noreply.github.com>
2024-12-27 21:22:34 +0100

    Clear

--- src/main/java/tn/mbs/ascendantmobs/procedures/ReturnMaxMinLevelsProcedure.java
+++
@@ -1,9 +0,0 @@
-package tn.mbs.ascendantmobs.procedures;
-
-import net.minecraft.world.level.LevelAccessor;
-
-public class ReturnMaxMinLevelsProcedure {
-	public static String execute(LevelAccessor world) {
-		return "[" + new java.text.DecimalFormat("##").format(GetBaseLevelFromConfigProcedure.execute(world)) + " - " + new java.text.DecimalFormat("##").format(GetMaxLevelFromConfigProcedure.execute(world)) + "]";
-	}
-}
