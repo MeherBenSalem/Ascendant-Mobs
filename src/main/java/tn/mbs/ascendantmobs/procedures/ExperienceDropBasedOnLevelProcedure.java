3f85e0d98dec9f8ab1837c1600712a5b95908fc8
MeherBenSalem <74470806+MeherBenSalem@users.noreply.github.com>
2024-12-31 23:52:33 +0100

    1.1.2

--- src/main/java/tn/mbs/ascendantmobs/procedures/ExperienceDropBasedOnLevelProcedure.java
+++ src/main/java/tn/mbs/ascendantmobs/procedures/ExperienceDropBasedOnLevelProcedure.java
@@ -1,5 +1,6 @@
 package tn.mbs.ascendantmobs.procedures;
 
+import tn.mbs.ascendantmobs.configuration.MobsListConfigConfiguration;
 import tn.mbs.ascendantmobs.configuration.MobsLevelsMainConfigConfiguration;
 
 import net.minecraftforge.fml.common.Mod;
@@ -7,7 +8,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
+import net.minecraft.world.scores.Team;
+import net.minecraft.world.scores.PlayerTeam;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
@@ -31,6 +36,23 @@ public class ExperienceDropBasedOnLevelProcedure {
 		if (entity == null)
 			return;
 		if (entity.getPersistentData().getBoolean("am_level")) {
+			if (!MobsListConfigConfiguration.DEATH_MESSAGES.get()) {
+				if (world instanceof Level _level) {
+					PlayerTeam _pt = _level.getScoreboard().getPlayerTeam("am_no_msg");
+					if (_pt != null)
+						_pt.setDeathMessageVisibility(Team.Visibility.NEVER);
+				}
+				{
+					Entity _entityTeam = entity;
+					PlayerTeam _pt = _entityTeam.level().getScoreboard().getPlayerTeam("am_no_msg");
+					if (_pt != null) {
+						if (_entityTeam instanceof Player _player)
+							_entityTeam.level().getScoreboard().addPlayerToTeam(_player.getGameProfile().getName(), _pt);
+						else
+							_entityTeam.level().getScoreboard().addPlayerToTeam(_entityTeam.getStringUUID(), _pt);
+					}
+				}
+			}
 			if (world instanceof ServerLevel _level)
 				_level.addFreshEntity(new ExperienceOrb(_level, x, y, z, (int) Math.floor(entity.getPersistentData().getDouble("am_lvl") * (double) MobsLevelsMainConfigConfiguration.XP_MODFIER.get())));
 		}
