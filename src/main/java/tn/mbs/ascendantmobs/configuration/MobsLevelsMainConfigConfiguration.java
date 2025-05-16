3eed8d9aec38592795fa7c87745954e263630d3d
MeherBenSalem <74470806+MeherBenSalem@users.noreply.github.com>
2025-05-16 17:21:15 +0100

    Neat

--- src/main/java/tn/mbs/ascendantmobs/configuration/MobsLevelsMainConfigConfiguration.java
+++ src/main/java/tn/mbs/ascendantmobs/configuration/MobsLevelsMainConfigConfiguration.java
@@ -19,12 +19,6 @@ public class MobsLevelsMainConfigConfiguration {
 	public static final ForgeConfigSpec.ConfigValue<Double> RANDOM_EFFECTS_LEVEL;
 	public static final ForgeConfigSpec.ConfigValue<Double> RANDOM_EFFECTS_CHANCE;
 	public static final ForgeConfigSpec.ConfigValue<String> ASCENDANT_LOOT_TABLE;
-	public static final ForgeConfigSpec.ConfigValue<Boolean> DISPLAY_LVL_OVERLAY;
-	public static final ForgeConfigSpec.ConfigValue<Boolean> ENABLE_NAME_RENDER;
-	public static final ForgeConfigSpec.ConfigValue<Double> Y_OFFSET;
-	public static final ForgeConfigSpec.ConfigValue<Double> X_OFFSET;
-	public static final ForgeConfigSpec.ConfigValue<Double> Y_OFFSET_LVL;
-	public static final ForgeConfigSpec.ConfigValue<String> LEVEL_COLOR_CODE;
 	static {
 		BUILDER.push("Attributes Scale Settings");
 		ATTRIBUTES_LIST = BUILDER.comment("Configures attribute scaling with mob levels: [attribute] defines the stat, [value] is per-level increment, [max] is the cap, [mob] (optional) targets specific mobs or all").defineList("attributes_list",
@@ -45,14 +39,6 @@ public class MobsLevelsMainConfigConfiguration {
 		RANDOM_EFFECTS_LEVEL = BUILDER.comment("[1-200]").define("ascendant_effects_min_level", (double) 25);
 		RANDOM_EFFECTS_CHANCE = BUILDER.comment("[1-100]").define("ascendant_effects_chance", (double) 0.5);
 		ASCENDANT_LOOT_TABLE = BUILDER.define("ascendant_loot_table", "minecraft:chests/end_city_treasure");
-		BUILDER.pop();
-		BUILDER.push("Ingame Overlay Settings");
-		DISPLAY_LVL_OVERLAY = BUILDER.define("display_lvl_overlay", true);
-		ENABLE_NAME_RENDER = BUILDER.comment("if your gonna disable this its recommended you reset the level_x_offset_level to 0 ").define("enable_name_render", false);
-		Y_OFFSET = BUILDER.define("name_y_offset", (double) 0.4);
-		X_OFFSET = BUILDER.define("level_x_offset", (double) 0);
-		Y_OFFSET_LVL = BUILDER.define("level_y_offset", (double) 0.7);
-		LEVEL_COLOR_CODE = BUILDER.define("level_color_code", "§2");
 		BUILDER.pop();
 
 		SPEC = BUILDER.build();
