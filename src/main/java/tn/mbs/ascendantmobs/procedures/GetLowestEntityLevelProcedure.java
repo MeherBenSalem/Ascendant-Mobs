7bc6f4f173a44671d7d21b891bde5161ecd348c9
Rogevehnam <rogevehnam@gmail.com>
2025-08-18 21:13:07 +0300

    fix: MOTP level scaling by enlarging player search radius to 128 blocks and ignoring zero levels

--- src/main/java/tn/mbs/ascendantmobs/procedures/GetLowestEntityLevelProcedure.java
+++ src/main/java/tn/mbs/ascendantmobs/procedures/GetLowestEntityLevelProcedure.java
@@ -14,12 +14,14 @@ public class GetLowestEntityLevelProcedure {
 	public static double execute(LevelAccessor world, double x, double y, double z) {
 		double lowestLevel = 0;
 		lowestLevel = 9999;
+		final double searchRadius = 128d;
 		{
 			final Vec3 _center = new Vec3(x, y, z);
-			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(20 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+			List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(searchRadius), e -> true).stream()
+					.sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
 			for (Entity entityiterator : _entfound) {
 				if (entityiterator instanceof Player || entityiterator instanceof ServerPlayer) {
-					if (entityiterator.getPersistentData().getDouble("motp_level") < lowestLevel) {
+					if (entityiterator.getPersistentData().getDouble("motp_level") < lowestLevel && entityiterator.getPersistentData().getDouble("motp_level") > 0) {
 						lowestLevel = entityiterator.getPersistentData().getDouble("motp_level");
 					}
 				}
